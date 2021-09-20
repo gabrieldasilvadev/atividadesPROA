d886ee098537257247a20f644607ed7a25d89f29
Gabriel <gabrieldasilvaprivado1@gmail.com>
2021-09-20 10:02:06 -0300

    Envia alteracao no diretorio das imagens

--- light_to_dark/src/js/alycia.js
+++ light_to_dark/src/js/alycia.js
@@ -1,25 +1,30 @@
 const html = document.querySelector("html")
 const checkbox = document.querySelector("input[name=theme]")
 
-const getStyle = (element, style) => {
+const getStyle = (element, style) => 
     window
         .getComputedStyle(element)
         .getPropertyValue(style)
 
-}
 
 const initialColors = {
     bg: getStyle(html, "--bg"),
+    bgPanelone: getStyle(html, "--bgpanel-one"),
+    bgPaneltwo: getStyle(html, "--bgpanel-two"),
+    bgPanelthree: getStyle(html, "--bgpanel-three"),
+    bgPanelfour: getStyle(html, "--bgpanel-four"),
+    colorHeadings: getStyle(html, "--color-headings"),
     colorText: getStyle(html, "--color-text"),
-    bgQuote: getStyle(html, "--bg-quote"),
-    textQuote: getStyle(html, "--text-quote"),
 }
 
 const darkMode = {
-    bg: "#1e1e1e",
-    colorText: "#FFFFFF",
-    bgQuote: "#1e1e1e",
-    textQuote: "#FFFFFF",
+    bg: "#000000",
+    bgPanelone: "#434343",
+    bgPaneltwo: "#4B0052",
+    bgPanelthree: "#470028",
+    bgPanelfour: "#002750",
+    colorHeadings: "#7B0034",
+    colorText: "#FFFFFF"
 }
 
 const transformKey = key => 
@@ -36,4 +41,3 @@ const changeColors = (colors) => {
 checkbox.addEventListener("change", ({target}) => {
     target.checked ? changeColors(darkMode) : changeColors(initialColors)
 })
-
